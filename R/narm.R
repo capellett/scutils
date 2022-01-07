58e1c9eb72012ffbbaba8c0877218386bff2d2db
Alex Pellett <PellettC@dnr.sc.gov>
2022-01-07 15:54:21 -0500

    remove the hello function documentation

--- R/narm.R
+++ R/narm.R
@@ -1,9 +1,6 @@
-
 #' @title Remove Missing Values
 #' @description Remove missing values (NA) from a vector
-#
 #' @param x a vector.
 #' @return A vector the same as x but with missing values removed.
 #' @export
-#'
 na.rm <- function(x) x[!is.na(x)]
