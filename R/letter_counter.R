b8f22e687b1bf8b707966b99ce319a97c7fceae3
Alex Pellett <PellettC@dnr.sc.gov>
2023-10-18 10:09:10 -0400

    try and fail to vectorize letter_counter

--- R/letter_counter.R
+++ R/letter_counter.R
@@ -8,3 +8,19 @@ letter_counter <- function(n) {
   if(n<1) return(NULL)
   lettr_countr(n)
 }
+
+### I can't get it to work with length > 1
+
+# lettr_countr2 <- function(n) {
+#   dplyr::if_else(
+#     n > 26, lettr_countr2(as.integer(n%/%26)))
+#   cat(LETTERS[n %% 26])
+# }
+#
+#
+# letter_counter2 <- function(n) {
+#   dplyr::if_else(
+#     n<1, NA_character_, lettr_countr2(n))
+#   }
+#
+# letter_counter2(c(1,2,3,27))
